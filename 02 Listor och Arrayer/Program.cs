c666dc1e1c4f2193de87452983de1ce96fdc1038
Alice Grissom <Alice.grissom@elev.ga.ntig.se>
2024-11-14 10:57:18 +0100

    first commit

---
+++ 02 Listor och Arrayer/Program.cs
@@ -0,0 +1,47 @@
+﻿// string [] leksaker = new string[5];
+// leksaker[0] = "Lego";
+// leksaker[1] = "Gosedjur";
+// leksaker[2] = "Pussel";
+// leksaker[3] = "Dockor";
+// leksaker[4] = "Pokémon";
+
+// // foreach (string leksak in leksaker)
+// // {
+// //     Console.WriteLine(leksak);
+// // }
+
+// // Console.ReadLine();
+
+// string [] klasskamrater = {"Melis", "Elsa", "Teodora", "Amira", "Vilma"};
+
+// int [] värden = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+// for (int i = 0; i < klasskamrater.Length; i++)
+// {
+//     Console.WriteLine($"{klasskamrater[i]} ger {leksaker[i]} värdet {värden[i]}");
+// }
+
+// Console.ReadLine();
+
+
+
+List<string> cities = new List<string>();
+string city = "";
+
+while (city != "exit")
+{
+    city = Console.ReadLine();
+
+    if (city != "exit")
+    {
+        cities.Add(city);
+    }
+}
+
+foreach (var c in cities)
+{
+    Console.WriteLine(c);
+}
+
+
+Console.ReadLine();
